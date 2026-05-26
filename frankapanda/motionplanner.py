887f06ce56cb249330eb773d79ea763fc5236c50
amli <amberli2@andrew.cmu.edu>
2026-05-26 16:38:16 -0400

    remove camera stand 2 as obstacle

--- frankapanda/motionplanner.py
+++ frankapanda/motionplanner.py
@@ -40,7 +40,9 @@ torch.backends.cudnn.benchmark = True
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 
-EE_LINK_CENTER_TO_GRIPPER_TIP = 0.10
+EE_LINK_CENTER_TO_GRIPPER_TIP = 0.10  # for ball in ring task
+# EE_LINK_CENTER_TO_GRIPPER_TIP = 0.09  # for cutting dough task
+
 
 # Constants for motion planning
 CUROBO_ASSETS_PATH = "visplan/submodules/curobo/src/curobo/content/assets/"       # Have to save here because cuRobo looks for mesh obstacles here
@@ -194,7 +196,7 @@ class MotionPlanner:
         self.world_config.add_obstacle(self.back_wall)
         self.world_config.add_obstacle(self.table)
         self.world_config.add_obstacle(self.camera_stand1)
-        self.world_config.add_obstacle(self.camera_stand2)
+        # self.world_config.add_obstacle(self.camera_stand2)
         # self.world_config.add_obstacle(self.pointcloud_mesh)
 
         motion_gen_config = MotionGenConfig.load_from_robot_config(
